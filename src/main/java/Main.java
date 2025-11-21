4e49a5380bc11a0ef1c1d82ca9ab09a59bf216fc
AvailableNow <currently.out.of.service@gmail.com>
2025-11-20 22:31:48 -0800

    Version 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
         String winner = "";
-        int speedCar = 0;
-        String makeCar = "";
+        String carMake = "";
+        int carSpeed = 0;
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i <= 2; i++) {
-            Scanner scanner = new Scanner(System.in);
             System.out.printf("Please enter the car make #%d.%n", i+1);
-            makeCar = scanner.next();
+            carMake = scanner.next();
             System.out.printf("Please enter the vehicle speed #%d.%n", i+1);
             while (true) {
-                    if (scanner.hasNextInt()) {
-                        speedCar = scanner.nextInt();
-                        if ((speedCar >= Vehicle.MINSPEED) && (speedCar <= Vehicle.MAXSPEED)) {
-                            break;
-                        } else {
-                            System.out.printf("Speed must be between %d and %d.%n", Vehicle.MINSPEED, Vehicle.MAXSPEED);
-                            System.out.printf("Please enter the vehicle speed #%d.%n", i+1);
-                        }
+                if (scanner.hasNextInt()){
+                    carSpeed = scanner.nextInt();
+                    if ((carSpeed >= Vehicle.MINSPEED) && (carSpeed <= Vehicle.MAXSPEED)) {
+                        break;
                     } else {
-                        System.out.println("Invalid input - please enter a number.");
-                        scanner.next();
-                        System.out.printf("Please enter the vehicle speed #%d.%n", i + 1);
+                        System.out.printf("Speed must be between %d and %d.%n", Vehicle.MINSPEED, Vehicle.MAXSPEED);
+                        System.out.printf("Please enter the vehicle speed #%d.%n", i+1);
+                    }
+                } else {
+                    System.out.println("Invalid input - please enter a number.");
+                    scanner.next();
+                    System.out.printf("Please enter the vehicle speed #%d.%n", i + 1);
                 }
             }
+            winner = new Race (new Vehicle(carMake, carSpeed)).leader();
+            System.out.printf("The fastest car: %s%n", winner);
         }
-        winner  = new Race(new Vehicle(makeCar, speedCar)).leader();
-        System.out.printf("The fastest car: %s%n", winner);
     }
 }
